2463d0b2cb6720cadc400c5117d54801b1cfc781
EstickSB <esticksilva4@gmail.com>
2026-02-09 18:13:51 -0500

    improvement in watermark

--- src/components/Watermark.tsx
+++ src/components/Watermark.tsx
@@ -8,8 +8,8 @@ export const Watermark: React.FC<WatermarkProps> = ({ textColor = 'white' }) => 
   return (
     <div className="flex px-10 items-center gap-1 opacity-60 mix-blend-screen">
       <span
-        className="text-[10px] uppercase tracking-[0.3em] font-sans font-medium"
-        style={{ color: textColor }}
+        className="text-[10px] tracking-[0.3em] font-sans font-medium"
+        style={{ color: textColor }}  
       >
         LaCarta<span className="text-red-500 text-lg leading-none">.</span>
       </span>
